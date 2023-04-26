5152ccaf9f8c68b44db71ab210709599632944f4
chenyu-jiang <jchenyu@connect.hku.hk>
2023-04-26 09:02:30 +0000

    revert to torch jit script due to compile issues

--- megatron/model/utils.py
+++ megatron/model/utils.py
@@ -40,8 +40,8 @@ def get_linear_layer(rows, columns, init_method):
         layer.bias.zero_()
     return layer
 
-# @torch.jit.script
-@torch.compile(dynamic=True)
+# @torch.compile(dynamic=True)
+@torch.jit.script
 def gelu_impl(x):
     """OpenAI's gelu implementation."""
     return 0.5 * x * (1.0 + torch.tanh(0.7978845608028654 * x *
@@ -50,7 +50,7 @@ def openai_gelu(x):
     return gelu_impl(x)
 
 #This is actually Python equivalent of torch.nn.functional.gelu(), also with type hints for ONNX exporter
-# @torch.jit.script
-@torch.compile(dynamic=True)
+# @torch.compile(dynamic=True)
+@torch.jit.script
 def erf_gelu(x):
     return x * 0.5 * (torch.erf(x / 1.41421).to(dtype=x.dtype)+torch.ones_like(x).to(dtype=x.dtype))
