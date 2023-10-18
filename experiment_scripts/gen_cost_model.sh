a02ddf24135ae077deb48ca86211d22fbc637603
chenyu-jiang <jchenyu@connect.hku.hk>
2023-10-18 13:00:03 +0000

    bug fix in gen_cost_model.sh and run_ablation.sh

--- experiment_scripts/gen_cost_model.sh
+++ experiment_scripts/gen_cost_model.sh
@@ -1,6 +1,6 @@
 echo "Creating cost models for GPT and T5 11B, using 4 different tensor parallel degrees (1, 2, 4, 8 GPUs)." 
 python3 run_cost_model_benchmarks.py --out_dir ./cost_model_gpt_6.7b --model_config ./experiment_configs/model_configs/gpt_6.7b_16l.json
-python3 run_cost_model_benchmarks.py --out_dir ./cost_model_t5_11b --model_config ./experiment_configs/model_configs/gpt_6.7b_16l.json
+python3 run_cost_model_benchmarks.py --out_dir ./cost_model_t5_11b --model_config ./experiment_configs/model_configs/t5_11b_12l.json
 mkdir -p ./cost_models
 python3 gen_cost_model_from_profile.py --profile_dir ./cost_model_gpt_6.7b --out_path ./cost_models/gpt_6.7b_cm.pkl
 python3 gen_cost_model_from_profile.py --profile_dir ./cost_model_t5_11b --out_path ./cost_models/t5_11b_cm.pkl
